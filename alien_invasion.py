9d2f23817863cde64929e14722d9539597b3d107
Nickon <nickonfo@gmail.com>
2019-09-17 09:51:56 +0100

    Create bullet object and settings, allow it to react to spacebar key press to fire

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,5 +1,5 @@
 import pygame
-
+from pygame.sprite import Group
 from settings import Settings
 from ship import Ship
 import game_functions as gf
@@ -15,11 +15,14 @@ def rungame():
     # Draw ship
     ship = Ship(ai_settings, screen)
 
+    # Make a group to store bullets
+    bullets = Group()
+
     # Start the main loop for the game
     while True:
-        gf.check_events(ship)
+        gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
-        gf.update_screen(ai_settings, screen, ship)
-
+        bullets.update()
+        gf.update_screen(ai_settings, screen, ship, bullets)
 
 rungame()
